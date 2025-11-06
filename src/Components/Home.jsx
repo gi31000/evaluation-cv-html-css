780be487496b6a5844b1667dde9d4a7a1911c446
Gi31-Trucks <gino.crespin@gmail.com>
2025-11-06 23:57:50 +0100

    Working on Navbar & structure pages.

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <main className="container">
+            
+            <img src="/img/hero-bg.jpg" alt="Dev pc"/>
+            <p>Bienvenue sur ma page d'acceuil</p>
+        </main>
+    );
+};
+
+export default Home;
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,26 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Acceuil</Link>
-                    </li>
-                    <li>
-                        <Link to="/apropos">Qui suis-je ?</Link>
-                    </li>
-                    <li>
-                        <Link to="/horloge">Horloge</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Acceuil</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/mentions">Mentions</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
